bfbbd52cb12ba7e3152ec22df0bbd53bdec687b8
Karl Tietz <ktietz@gmail.com>
2013-10-15 09:58:44 -0400

    Show description in main restaurant view. Cut off extraneous text in description. Make back button div have more padding to make it a bigger target for touchscreens.

--- www/js/views.js
+++ www/js/views.js
@@ -48,8 +48,33 @@ App.RestaurantListItemView = Backbone.Marionette.ItemView.extend({
 });
 
 App.RestaurantInfoView = Backbone.Marionette.ItemView.extend({
-    template: '#restaurant-page-tpl'
+    template: '#restaurant-page-tpl',
+    ui: {
+        phone : '.phone',
+        email : '.email',
+        website : '.website',
+        address : '.address'
+    },
+    onRender: function(){
+       App.HideBlankInformation(this);
+    }
+
 });
+
+App.HideBlankInformation = function(view) {
+    if (view.model.get('telephone') === ""){
+        view.ui.phone.addClass('hidden');
+    }
+    if (view.model.get('email') === ""){
+        view.ui.email.addClass('hidden');
+    }
+    if (view.model.get('website') === ""){
+        view.ui.website.addClass('hidden');
+    }
+    if (view.model.get('address') === ""){
+        view.ui.address.addClass('hidden');
+    }
+};
 
 // Accommodations --------------------------------------------------------
 App.AccommodationsListView = Backbone.Marionette.CollectionView.extend({
@@ -65,7 +90,16 @@ App.AccommodationsListItemView = Backbone.Marionette.ItemView.extend({
 });
 
 App.AccommodationInfoView = Backbone.Marionette.ItemView.extend({
-    template: '#accommodation-page-tpl'
+    template: '#accommodation-page-tpl',
+    ui: {
+        phone : '.phone',
+        email : '.email',
+        website : '.website',
+        address : '.address'
+    },
+    onRender: function(){
+        App.HideBlankInformation(this);
+    }
 });
 
 
@@ -83,7 +117,16 @@ App.AttractionsListItemView = Backbone.Marionette.ItemView.extend({
 });
 
 App.AttractionInfoView = Backbone.Marionette.ItemView.extend({
-    template: '#attraction-page-tpl'
+    template: '#attraction-page-tpl',
+    ui: {
+        phone : '.phone',
+        email : '.email',
+        website : '.website',
+        address : '.address'
+    },
+    onRender: function(){
+        App.HideBlankInformation(this);
+    }
 });
 
 
